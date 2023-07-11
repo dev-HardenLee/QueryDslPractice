ff46177a90d77ab379f3651ca7e0c72abe419c83
harden <harden.lee@companywe.co.kr>
2023-07-11 17:12:13 +0900

    practice start..

---
+++ src/main/java/com/example/test/QueryDslPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslPracticeApplication.class, args);
+	}
+
+}
